fbf513972272e5313572f5b33661c75e075f6cc0
Jungho Lee <nightfallb612@gmail.com>
2024-03-12 21:58:30 -0700

    crawling and save

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 async function run() {
   // Launch the browser and open a new blank page
@@ -7,10 +8,37 @@ async function run() {
   // Navigate the page to a URL
   await page.goto("https://vibe.naver.com/chart/total");
 
+  // const songs = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll(".tracklist tbody tr"), (e) => ({
+  //     rank: e.querySelector(".rank span").innerText,
+  //     song: e.querySelector(".song .link_text").innerText,
+  //     artist: e.querySelector(".artist .link_artist span").innerText,
+  //   }))
+  // );
+
   const links = await page.evaluate(() =>
     Array.from(document.querySelectorAll("a"), (e) => e.href)
   );
   console.log(links);
+
+  const songs = await page.$$eval(".tracklist tbody tr", (elements) =>
+    elements.map((e) => ({
+      rank: e.querySelector(".rank span").innerText,
+      song: e.querySelector(".song .link_text").innerText,
+      artist: e.querySelector(".artist .link_artist span").innerText,
+    }))
+  );
+  console.log(songs);
+
+  // fs.writeFile("top100.json", JSON.stringify(songs), (err) => {
+  //   if (err) throw err;
+  //   console.log("File saved successfuly");
+  // });
+
+  fs.writeFile("links.json", JSON.stringify(links), (err) => {
+    if (err) throw err;
+    console.log("File saved successfuly");
+  });
 
   await browser.close();
 }
